99301184be9efb564c9cb36c06103e52114622f7
NikkeMan <nikoleppanoro@outlook.com>
2019-01-04 11:36:34 +0200

    Checkpoint stuff
    - prefab for checkpoints
    - CheckpointScript.cs
    - scene for testing
    - folders for prefabs & scripts

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour {
+
+    private Vector2 spawnPoint;
+
+	// Use this for initialization
+	void Start () {
+        spawnPoint = gameObject.GetComponentInChildren<Transform>().transform.position;
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Entered " + gameObject.name);
+
+        if (other.gameObject.tag == "Player")
+        {
+            CheckpointHandler.lastCheckpoint = spawnPoint;
+            //TODO: enter safezone?
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //TODO: exit safezone?
+        }
+    }
+}
