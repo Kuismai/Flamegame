e3691b95b3ab4d225743cea18def19ee2ca03d8e
JeKoski <jesse.koski90@gmail.com>
2019-01-16 10:29:43 +0200

    Dripping Water working

---
+++ Assets/Scripts/WaterDropDamage.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropDamage : MonoBehaviour
+{
+    [SerializeField] float dropletDamage = 40f;
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player") // If the colliding object has the Player-tag
+        {
+            PlayerGPMechanics.playerHealth -= dropletDamage;
+            Destroy(gameObject);
+        }
+
+        else if (collision.gameObject.tag == "DropletExit")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Scripts/WaterDropSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropSpawner : MonoBehaviour
+{
+    public GameObject droplet;
+    [SerializeField] float spawnDelay = 2f;
+    private float spawnTimer = 0;
+
+    private void FixedUpdate()
+    {
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer > spawnDelay)
+        {
+            Instantiate(droplet, transform.position, Quaternion.Euler(0, 0, 0));
+            spawnTimer = 0;
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
