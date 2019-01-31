a8808273e73656a7cf789e3b3e5d06f224ba00bb
JeKoski <jesse.koski90@gmail.com>
2019-01-31 12:10:53 +0200

    TemplateLevelFilling

---
+++ Assets/Scripts/MatchScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchScript : MonoBehaviour
+{
+
+    GameObject fire;
+
+    private void Awake()
+    {
+        fire = GameObject.Find("Fire");
+        fire.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "OverheatHitbox")
+        {
+            fire.SetActive(true);
+        }
+    }
+}
