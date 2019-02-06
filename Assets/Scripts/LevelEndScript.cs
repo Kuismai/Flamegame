48f51c0d5d432e704f3711aa3f76146ec870294d
JeKoski <jesse.koski90@gmail.com>
2019-02-06 10:59:46 +0200

    IntroLevelContCont

---
+++ Assets/Scripts/LevelEndScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelEndScript : MonoBehaviour
+{
+    public static bool levelComplete = false;
+    public int levelToLoad;
+    private float endTimer;
+    public float endDelay = 2f;
+
+    private void Awake()
+    {
+        endTimer = 0f;
+    }
+
+    private void Update()
+    {
+        if (levelComplete)
+        {
+            endTimer += Time.deltaTime;
+        }
+
+        if (levelComplete && endTimer >= endDelay)
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player") // If the object colliding with resource pickup has the Player-tag
+        {
+            levelComplete = true;
+        }
+    }
+
+}
